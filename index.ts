21a8dcb1e02506c1f321c2ab283bd8ec3aa67eba
hifisaputra <hifisaputra1@gmail.com>
2025-03-05 15:45:26 +0800

    batman

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const ENDPOINT = "https://api.test.paysafe.com/paymenthub/v1";
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/proxy", async (req: Request, res: Response) => {
+  const data = req.body;
+  const url = `${ENDPOINT}/${data.endpoint}`;
+  const token = req.headers.authorization?.split(" ")[1];
+
+  console.log("Proxy request", data, token);
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data.body),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Basic ${token}`,
+    },
+  });
+
+  const responseData = await response.json();
+
+  res.json({ message: "API Call Success", data: responseData });
+});
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ next.config.js
@@ -0,0 +1,34 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:3000",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default config;
