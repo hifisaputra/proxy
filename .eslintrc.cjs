c6844d1e2cc7a675b961b35a1a3f2d48ec3335d2
hifisaputra <hifisaputra1@gmail.com>
2025-03-05 16:11:14 +0800

    turn off ts rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,7 @@ const config = {
     ],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
   },
 };
 module.exports = config;
