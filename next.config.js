b49146e32aee7150995d5f59853dff4da46f5c77
hifisaputra <hifisaputra1@gmail.com>
2025-03-05 16:06:33 +0800

    update proxy

--- next.config.js
+++ next.config.js
@@ -12,10 +12,7 @@ const config = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000",
-          }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
@@ -23,7 +20,7 @@ const config = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
           },
         ],
       },
--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -2,6 +2,18 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+
+  const { url, method, headers, data } = body;
+
   console.log(body);
-  return NextResponse.json({ message: "Hello World" });
+
+  const response = await fetch(url, {
+    method,
+    headers,
+    body: JSON.stringify(data),
+  });
+
+  const responseBody = await response.json();
+
+  return NextResponse.json(responseBody);
 }
